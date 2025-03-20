01932ff8058dfb772723618fbea251bdfe4ea66e
Lendly Cagata | Local Admin <lendly.cagata@telus.com>
2025-03-21 00:44:51 +0800

    feat: initial commit

---
+++ src/main/java/com/java/_1/my_demo_java_devops/MyDemoJavaDevopsApplication.java
@@ -0,0 +1,14 @@
+package com.java._1.my_demo_java_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDemoJavaDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDemoJavaDevopsApplication.class, args);
+		System.out.println("Hello Pogs!");
+	}
+
+}
---
+++ src/test/java/com/java/_1/my_demo_java_devops/MyDemoJavaDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java._1.my_demo_java_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoJavaDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
